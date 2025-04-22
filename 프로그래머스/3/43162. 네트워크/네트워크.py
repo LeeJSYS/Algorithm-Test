745cdadf02a2919f71dfbb7672f9bd5165dc7650
Lee Junseok <120862683+LeeJSYS@users.noreply.github.com>
2025-04-22 20:36:10 +0900

    [level 3] Title: 네트워크, Time: 0.75 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,19 @@
+def solution(n, computers):
+    visited=[False]*n
+    cnt=0
+    
+    def dfs(node):
+        if not visited[node]:
+            visited[node]=True
+            for i in range(n):
+                if computers[node][i]>0:
+                    dfs(i)
+            
+    
+    for j in range(n):
+        if not visited[j]:
+            cnt+=1
+            dfs(j)
+    return cnt
+    
+        
