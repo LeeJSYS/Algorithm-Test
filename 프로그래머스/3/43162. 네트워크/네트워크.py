687dcbb2aaf1c4c6178bd030261a2d9c3d824227
Lee Junseok <120862683+LeeJSYS@users.noreply.github.com>
2025-05-02 17:52:49 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,32 @@
-def solution(n, computers):
-    visited=[False]*n
-    cnt=0
+# def solution(n, computers):
+#     visited=[False]*n
+#     cnt=0
     
-    def dfs(node):
-        if not visited[node]:
-            visited[node]=True
-            for i in range(n):
-                if computers[node][i]>0:
-                    dfs(i)
+#     def dfs(node):
+#         if not visited[node]:
+#             visited[node]=True
+#             for i in range(n):
+#                 if computers[node][i]>0:
+#                     dfs(i)
             
     
-    for j in range(n):
-        if not visited[j]:
-            cnt+=1
-            dfs(j)
-    return cnt
+#     for j in range(n):
+#         if not visited[j]:
+#             cnt+=1
+#             dfs(j)
+#     return cnt
     
-        
+def solution(n,computers):
+    visited=[False]*n
+    cnt=0
+    def dfs(node):
+        visited[node] = True
+        for i in range(n):
+            if computers[node][i]==1 and not visited[i]:
+                visited[i] = True
+                dfs(i)
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            cnt +=1
+    return cnt
