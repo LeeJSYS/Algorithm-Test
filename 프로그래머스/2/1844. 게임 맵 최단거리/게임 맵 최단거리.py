fa9c8b0003e0128bfac5a8806a58fa56077bb19b
Lee Junseok <120862683+LeeJSYS@users.noreply.github.com>
2025-05-02 15:06:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 28.86 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,39 +1,69 @@
-from collections import deque
+from  collections import deque
+
 def solution(maps):
-    n=len(maps)
-    m=len(maps[0])
+    n = len(maps)
+    m = len(maps[0])
     visited=[[False for _ in range(m)] for _ in range(n)]
-    result=[]
-    
-    def bfs(node,cnt):
-        q = deque()
-        q.append([node[0],node[1],cnt])
-        visited[node[0]][node[1]]=True
+    def bfs(a,b,cnt):
+        q = deque([(a,b,cnt)])
+        visited[a][b]=True
         while q:
-            x,y,cnts=q.popleft()
-            dx=[-1,1,0,0]#상하좌우
-            dy=[0,0,-1,1]
-            for i in range(4):
-                nx=x+dx[i]
-                ny=y+dy[i]
-                if nx<0 or nx>=n or ny<0 or ny>=m:
+            x,y,cnt = q.popleft()
+            for move in ((0,-1), (-1,0), (0,1),(1,0)):
+                nx = x + move[0]
+                ny = y + move[1]
+                if nx <0 or nx >= n or ny <0 or ny >= m:
                     continue
-                if maps[nx][ny]==0:
+                if maps[nx][ny]==0 or visited[nx][ny]:
                     continue
-                if maps[nx][ny]==1 and not visited[nx][ny]:
-                    maps[nx][ny]=-cnts
-                    if nx==n-1 and ny==m-1:
-                        result.append(cnts)
-                    else:
-                        visited[nx][ny]=True
-                        q.append([nx,ny,cnts+1])
-                    
-    bfs((0,0),2)
-    
-    if len(result)==0:
+
+                visited[nx][ny] = True
+                q.append([nx,ny,cnt+1])
+                if nx==n-1 and ny==m-1:
+                    return cnt+1
+    result = bfs(0,0,1)
+    if result == None:
         return -1
     else:
-        return min(result)
+        return result
+    
+    
+# from collections import deque
+# def solution(maps):
+#     n=len(maps)
+#     m=len(maps[0])
+#     visited=[[False for _ in range(m)] for _ in range(n)]
+#     result=[]
+    
+#     def bfs(node,cnt):
+#         q = deque()
+#         q.append([node[0],node[1],cnt])
+#         visited[node[0]][node[1]]=True
+#         while q:
+#             x,y,cnts=q.popleft()
+#             dx=[-1,1,0,0]#상하좌우
+#             dy=[0,0,-1,1]
+#             for i in range(4):
+#                 nx=x+dx[i]
+#                 ny=y+dy[i]
+#                 if nx<0 or nx>=n or ny<0 or ny>=m:
+#                     continue
+#                 if maps[nx][ny]==0:
+#                     continue
+#                 if maps[nx][ny]==1 and not visited[nx][ny]:
+#                     maps[nx][ny]=-cnts
+#                     if nx==n-1 and ny==m-1:
+#                         result.append(cnts)
+#                     else:
+#                         visited[nx][ny]=True
+#                         q.append([nx,ny,cnts+1])
+                    
+#     bfs((0,0),2)
+    
+#     if len(result)==0:
+#         return -1
+#     else:
+#         return min(result)
     
 #  GPT 답안
 # from collections import deque
